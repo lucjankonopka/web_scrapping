d23e999e7c46e3e793742a289189596b6aa2fa4d
Lucjan Konopka <111157625+lucjankonopka@users.noreply.github.com>
2023-06-23 11:53:14 +0200

    update of the code and results

--- collecting_data.py
+++ collecting_data.py
@@ -2,164 +2,171 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
-import sys
+
+### Functions for getting detail data
+
+# Function to extract restaurant name
+def get_name(soup):
+    
+    try:
+        link_cityzone = soup.find('section', class_ = 'block befi-address')
+        restaurant_name_tag = link_cityzone.find('div')
+        restaurant_name = restaurant_name_tag.text.strip()
+
+    except AttributeError:
+        restaurant_name = ""
+
+    return restaurant_name
+
+# Function to extract restaurant address
+def get_address(soup):
+    
+    try:
+        link_cityzone = soup.find('section', class_ = 'block befi-address')
+        restaurant_name_tag = link_cityzone.find('div')
+        restaurant_address_tag = restaurant_name_tag.find_next('div')
+        restaurant_address = restaurant_address_tag.text.strip()
+
+    except AttributeError:
+        restaurant_address = ""
+
+    return restaurant_address
+
+# Function to extract restaurant zip
+def get_zip(soup):
+    
+    try:
+        link_cityzone = soup.find('section', class_ = 'block befi-address')
+        restaurant_zip_tag = link_cityzone.find('span')
+        restaurant_zip = restaurant_zip_tag.text.strip()
+
+    except AttributeError:
+        restaurant_zip = ""
+
+    return restaurant_zip
+
+# Function to extract restaurant phone
+def get_phone(soup):
+    
+    try:
+        link_cityzone = soup.find('section', class_ = 'block befi-address')
+        restaurant_zip_tag = link_cityzone.find('span')
+        restaurant_phone_tag = restaurant_zip_tag.find_next('a')
+        restaurant_phone = restaurant_phone_tag.text.strip()
+        if not restaurant_phone.startswith("("):
+            restaurant_phone = "None"
+
+    except AttributeError:
+        restaurant_phone = "None"
+
+    return restaurant_phone
+
+# Function to extract restaurant type
+def get_type(link):
+    
+    try:
+        # Finding the restaurant type in http address (link)
+        text = (link.split("adressen/",1)[1]).split("/",1)[0]
+
+        # Splitting and merging text to get correct string type
+        splittet_text = text.split('-',1)
+        if len(splittet_text) == 1:
+            restaurant_type = splittet_text[0].capitalize()
+        else:
+            restaurant_type = splittet_text[0].capitalize() + ' ' + splittet_text[1].capitalize()
+
+    except AttributeError:
+        restaurant_type = "None"
+
+    return restaurant_type
+###
+
+if __name__ == '__main__':
+    
+    # Headers for request
+    HEADERS = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+    
+    # Selecting main page
+    page_url = "https://www.berlin.de/restaurants/stadtteile/"
+    page = requests.get(page_url, headers=HEADERS)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
 
 
-# Progress bar function
-# Source: https://stackoverflow.com/questions/3160699/python-progress-bar
+    # Finding links for each cityzone
+    page_list_zones = soup.find_all('h3', class_ = 'title')
 
-def progressbar(it, prefix="", size=60, out=sys.stdout):
-    count = len(it)
-    def show(j):
-        x = int(size*j/count)
-        print("{}[{}{}] {}/{}".format(prefix, "#"*x, "."*(size-x), j, count), 
-                end='\r', file=out, flush=True)
-    show(0)
-    for i, item in enumerate(it):
-        yield item
-        show(i+1)
-    print("\n", flush=True, file=out)
+    # Iterate over the <h3> elements and find the nested <a> elements
+    # and creating sublinks for each cityzone
+    zone_sublinks = []
 
+    for h3 in page_list_zones:
+        a_elements = h3.find_all('a')
+        for a in a_elements:
+            zone_sublinks.append(a['href'])
 
+    # Getting all subzones placed just in Berlin
+    zone_sublinks = zone_sublinks[:23]
 
-# Selecting main page using Beautiful Soup:
-page_url = "https://www.berlin.de/restaurants/stadtteile/"
-page = requests.get(page_url)
+    # Creating complete links for each cityzone
+    zone_links = []
 
-soup = BeautifulSoup(page.content, 'html.parser')
-#print(soup.prettify())
-
-# Finding links for each cityzone:
-page_list_zones = soup.find('ul', class_ = 'decoda-list')
-list_all_zones = page_list_zones.find_all('a', href=True)
-
-# Creating sublinks for each cityzone:
-zone_sublinks = []
-
-for el in list_all_zones:
-    zone_sublinks.append(el['href'])
-
-zone_sublinks
-
-# Creating complete links for each cityzone:
-zone_links = []
-
-for zone in zone_sublinks:
-    zone_page_url = 'https://www.berlin.de' + zone
-    zone_links.append(zone_page_url)
-
-zone_links
-
-
-# Creating empty list for dictionaries with restaurant informations:
-all_restaurants_list = []
-
-
-# Finding links for each restaurant in each cityzone:
-def find_restaurant_links(zone_link):
-    # Opening page of each restaurant:
-    zone_page = requests.get(zone_link)
-    local_soup = BeautifulSoup(zone_page.content, 'html.parser')
-    #print(local_soup.prettify())
-    # Finding links for each restarant from each zone by searching specific "text" in hrefs:
-    restaurant_sublink = local_soup.find_all('a', href=lambda href: href and "restaurants/adressen" in href and "html" in href, title=False)
-
-    # Creating temporary list of sublinks for each zone:
-    subzone_sublinks = []
-
-    # Adding all sublinks info list:
-    for el in restaurant_sublink:
-        subzone_sublinks.append(el['href'])
-
-    # Adding "htpps - prefix" for each link:
-    for zone in subzone_sublinks:
+    for zone in zone_sublinks:
         zone_page_url = 'https://www.berlin.de' + zone
-        all_restaurants_list.append(zone_page_url)
-    #print(len(all_restaurants_list))
+        zone_links.append(zone_page_url)
 
-    return all_restaurants_list
-
-
-# Looping through all zones:
-for link in zone_links:
-    find_restaurant_links(link)
-
-# Number of the restaurants:
-len(all_restaurants_list)
-
-# Creating the empty main_list where all info dictionaries will be collected:
-main_list = []
+    # Creating empty list for links of all restaurant separately
+    all_restaurants_list = []
 
 
-# Getting dictionaries data - name, type of restaurant, address, zip-code & phone: 
-def restaurant_info(restaurant):
-    
-    # Exploring restaurant subpage:
-    restaurant_page = requests.get(restaurant)
-    local_soup = BeautifulSoup(restaurant_page.content, 'html.parser')
-    #print(local_soup.prettify())
-   
-    # Finding data field:
-    restaurant_data = local_soup.find('div', class_ = 'befi-address')
+    # Finding links for each restaurant in each cityzone
+    def find_restaurant_links(zone_link):
+        # Opening page of each restaurant
+        zone_page = requests.get(zone_link)
+        local_soup = BeautifulSoup(zone_page.content, 'html.parser')
+        #print(local_soup.prettify())
+        # Finding links for each restarant from each zone by searching specific "text" in hrefs
+        restaurant_sublink = local_soup.find_all('a', href=lambda href: href and "restaurants/adressen" in href and "html" in href)
 
-    # Excluding name, address, zip code and phone from field:
-    restaurant_name_tag = restaurant_data.find('div')
-    restaurant_name = restaurant_name_tag.text.strip()
+        # Creating temporary list of sublinks for each zone
+        subzone_sublinks = []
 
-    restaurant_address_tag = restaurant_name_tag.find_next('div')
-    restaurant_address = restaurant_address_tag.text.strip()
+        # Adding all sublinks info list
+        for el in restaurant_sublink:
+            subzone_sublinks.append(el['href'])
 
-    restaurant_zip_tag = restaurant_data.find('span')
-    restaurant_zip = restaurant_zip_tag.text.strip()
+        # Adding "htpps - prefix" for each link
+        for zone in subzone_sublinks:
+            zone_page_url = 'https://www.berlin.de' + zone
+            all_restaurants_list.append(zone_page_url)
+        #print(len(all_restaurants_list))
 
-    restaurant_phone_tag = restaurant_zip_tag.find_next('a')
-    restaurant_phone = restaurant_phone_tag.text.strip()
-    # Checking if the phone number exists, otherwise writing "None" as a value:
-    if not restaurant_phone.startswith("("):
-        restaurant_phone = 'None'
-
-    # Finding the restaurant type in http address (link):
-    text = (restaurant.split("adressen/",1)[1]).split("/",1)[0]
-
-    # Splitting and merging text to get correct string type:
-    splittet_text = text.split('-',1)
-    if len(splittet_text) == 1:
-        new_name = splittet_text[0].capitalize()
-    else:
-        new_name = splittet_text[0].capitalize() + ' ' + splittet_text[1].capitalize()
+        return all_restaurants_list
 
 
-    # Creating dictionary with restaurant info according new created dataset:
-    restaurant_data[restaurant] = {'Name': restaurant_name,
-                        'Restaurant Type': new_name,
-                        'Address': restaurant_address,
-                        'Zip Code': restaurant_zip,
-                        'Phone': restaurant_phone}
-    
-    return restaurant_data[restaurant]
+    # Looping through all zones
+    for link in zone_links:
+        find_restaurant_links(link)
 
+    # Number of the restaurants
+    len(all_restaurants_list)
 
-# Looping through all restaurants and appending infos to new dataset:
-start_time = time.time()
+    # Creating the dictionary schema
+    result = {'Name': [], 'Restaurant_Type': [], 'Address': [], 'Zip_Code': [], 'Phone': []}
 
-for i in progressbar(range(len(all_restaurants_list)), "Computing: ", 50):
-    new_info = restaurant_info(all_restaurants_list[i])
-    # Appending the dictionary to the main_list:
-    main_list.append(new_info)
+    # Looping for extracting details from each link
+    for link in all_restaurants_list:
+        new_page = requests.get(link, headers=HEADERS)
+        new_soup = BeautifulSoup(new_page.content, 'html.parser')
 
-end_time =  time.time()
-elapsed_time = end_time - start_time
+        result['Name'].append(get_name(new_soup))
+        result['Restaurant_Type'].append(get_type(link))
+        result['Address'].append(get_address(new_soup))
+        result['Zip_Code'].append(get_zip(new_soup))
+        result['Phone'].append(get_phone(new_soup))
 
-print(f"Elapsed time: {round(elapsed_time,2)}s")
+    # Creating pandas dataframe
+    dataset_restaurants = pd.DataFrame.from_dict(result)
 
-# Checking list format and length:
-main_list
-len(main_list)
-
-# Creating new dataset for restaurants:
-table_view = {'Name': [], 'Restaurant_Type': [], 'Address': [], 'Zip_Code': [], 'Phone': []}
-
-dataset_restaurants = pd.DataFrame.from_records(main_list)
-
-# Exporting dataset as .csv file:
-dataset_restaurants.to_csv('dataset_berlin_restaurants.csv', index=False)
+    # Exporting dataset as .csv file
+    dataset_restaurants.to_csv('dataset_berlin_restaurants.csv', index=False)
